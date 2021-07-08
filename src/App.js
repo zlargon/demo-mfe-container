7b67a457dcbb904cac90e6acdc3efb77d4e0085b
zlargon <zlargon1988@gmail.com>
2021-07-07 20:28:25 -0400

    ModuleFederation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React from 'react';
+import Hello from 'COLCE/Hello';
 
 function App() {
   return (
     <div>
       <h1>App Shell</h1>
+      <Hello />
     </div>
   );
 }
